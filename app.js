1becfbc55ecb524e8a2847c2bb3eccb6baf48079
Bora Baflı <borabafli@gmail.com>
2022-05-09 00:47:39 +0300

    first basic api

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const db = require("./data/database");
+const quoteRoutes = require("./routes/quotes.routes");
+
+const app = express();
+
+app.use("/quote", quoteRoutes);
+
+app.use(function (error, req, res, next) {
+  res.status(500).json({
+    message: "something went wrong",
+  });
+});
+
+db.initDb()
+  .then(function () {
+    app.listen(3000);
+  })
+  .catch(function (error) {
+    console.log("database failed");
+  });
